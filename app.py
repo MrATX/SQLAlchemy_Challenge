a6086e273368f89cc418aa45fd2cb32d328346ff
unknown <johnwhitakeraustin@gmail.com>
2020-08-31 10:03:09 -0700

    Almost done with building the API

--- app.py
+++ app.py
@@ -36,16 +36,17 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"<a href='/api/v1.0/precipitation'>precipitation</a><br/>"
-        f"<a href='/api/v1.0/stations'>stations</a><br/>"
+        f"<a href='/api/v1.0/precipitation'>Precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>Stations</a><br/>"
         f"<a href='/api/v1.0/tobs'>Temperature</a><br/>"
-        f"<a href='/api/v1.0/2017-02-25'>Vaca Day 1 - 2/25</a><br/>"
+        f"<a href='/api/v1.0/20170225'>Vaca Day 1 - 2/25</a><br/>"
         f"<a href='/api/v1.0/2017-02-25/2017-03-4'>Whole Vaca 2/25 - 3/4</a><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
+    #Determine most recent date and date of one year ago
     last_date = session.query(Measurement,Measurement.date).order_by(Measurement.date.desc()).first()
     last_date_yr = last_date[1][0:4]
     last_date_mo = last_date[1][5:7]
@@ -75,15 +76,69 @@ def stations():
 
     return jsonify(all_results)
 
-#@app.root("/api/v1.0/tobs")
-#def tobs():
-    #session = Session(engine)
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    #Identify most active station
+    results = session.query(Measurement.station,func.count(Measurement.id)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.id).desc()).all()
+    most_active_stat = results[0][0]
+    #Determine most recent date and date of one year ago
+    last_date = session.query(Measurement,Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date_yr = last_date[1][0:4]
+    last_date_mo = last_date[1][5:7]
+    last_date_day = last_date[1][8:10]
+    last_date = dt.datetime(int(last_date_yr),int(last_date_mo),int(last_date_day))
+    year_ago = dt.datetime((int(last_date_yr)-1),int(last_date_mo),int(last_date_day))
+    #Query for last 12 months of temp observations
+    results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date>year_ago).\
+        filter(Measurement.station==most_active_stat).\
+        order_by(Measurement.date.desc()).all()
 
-#@app.root("/api/v1.0/<string:vaca_start>")
-#def 2017-02-25():
-    #session = Session(engine)
+    session.close()
 
-#@app.root("/api/v1.0/<string:vaca_start>/<string:vaca_end>")
+    all_results = []
+    for item in results:
+        item_dict = {"Date":item[0],"Temperature":round(item[1],2)}
+        all_results.append(item_dict)
+
+    return jsonify(all_results)
+
+@app.route("/api/v1.0/<string:vaca_start>")
+def vaca_start(vaca_start):
+    yr = vaca_start[0:4]
+    mo = vaca_start[4:6]
+    day = vaca_start[6:8]
+    vaca_start = (f"{yr}-{mo}-{day}")
+    session = Session(engine)
+    sel = [
+        Measurement.date,
+        func.min(Measurement.tobs),
+        func.avg(Measurement.tobs),
+        func.max(Measurement.tobs)
+    ]
+    results = session.query(*sel).\
+        filter(Measurement.date==vaca_start).\
+        order_by(Measurement.date).\
+        group_by(Measurement.date).all()
+    
+    session.close()
+
+    all_results = []
+    for item in results:
+        item_dict = {
+            "Date":item[0],
+            "Min Temp":item[1],
+            "Avg Temp":item[2],
+            "Max Temp":item[3],
+        }
+        all_results.append(item_dict)
+
+    return jsonify(all_results)
+
+#@app.route("/api/v1.0/<string:vaca_start>/<string:vaca_end>")
 #def tobs():
     #session = Session(engine)
 
